735e7f8ca478d36e5437fd4165e1ace94593c57f
kale1d <deneus@gmail.com>
2019-01-09 09:46:17 -0300

    added functionality in submenu

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,21 @@
 AOS.init();
+$('.hamburger--list .submenu--wrapper').hide();
+
 
 $('.hamburger--list .list--item').mouseover(function(){
-  const index = $(this).index()
+  const index = $(this).index();
+  if ($(this).hasClass('coaching')) {
+    console.log('yaaay')
+      $('.hamburger--list .submenu--wrapper').show();
+  } else {
+    $('.hamburger--list .submenu--wrapper').hide();
+  }
   $.each($('.item--img'), function (i, value) {
     if (index == i) {
-      $(this).removeClass('hidden')
-    } else {
-      $(this).addClass('hidden')
+      $(this).removeClass('hidden');
+    } 
+    else {
+      $(this).addClass('hidden');
     }
   });
 })
