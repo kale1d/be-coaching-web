ba02a5ebe8666b48d267d20e9c2c82e0eceafadc
kale1d <deneus@gmail.com>
2019-01-19 14:54:03 -0300

    final fixe

--- src/js/main.js
+++ src/js/main.js
@@ -28,6 +28,7 @@ $(document).ready(function(){
   
   // home
   $('#info--section').css('opacity', 0);
+  $('#values-list').css('opacity', 0);
   $('#coaching-title').css('opacity', 0);
   $('#left-item').css('opacity', 0);
   $('#center-item').css('opacity', 0);
@@ -38,7 +39,11 @@ $(document).ready(function(){
 
   $('#info--section').waypoint(function() {
       $('#info--section').addClass('fadeInUp');
-  }, { offset: '70%' });
+  }, { offset: '100%' });
+
+  $('#values-list').waypoint(function() {
+    $('#values-list').addClass('fadeInUp');
+}, { offset: '100%' });
 
   $('#coaching-title').waypoint(function() {
       $('#coaching-title').addClass('fadeInUp');
@@ -79,58 +84,59 @@ $('#organizacional').waypoint(function() {
 
  
 $('#dot').waypoint(function() {
-    $('#dot').addClass('bounce');
+    $('#dot').addClass('fadeIn');
 }, { offset: '70%' });
 
 $('#logo').css('opacity', 0);
  
 $('#logo').waypoint(function() {
     $('#logo').addClass('fadeInUp');
-}, { offset: '70%' });
-
+}, { offset: '40%' });
+$('#text-org').css('opacity', 0);
 $('#text-org').waypoint(function(){
-  $('#text-org').addClass('bounce');
-}, { offset: '70%' });
+  $('#text-org').addClass('fadeIn');
+}, { offset: '80%' });
  
 $('#executive').waypoint(function() {
     $('#executive').addClass('fadeIn');
 }, { offset: '70%' });
 
- 
+$('#dot-ex').css('opacity', 0);
 $('#dot-ex').waypoint(function() {
-    $('#dot-ex').addClass('bounce');
+    $('#dot-ex').addClass('fadeIn');
 }, { offset: '70%' });
 
 $('#logo-ex').css('opacity', 0);
  
 $('#logo-ex').waypoint(function() {
     $('#logo-ex').addClass('fadeInUp');
-}, { offset: '70%' });
+}, { offset: '40%' });
 
+$('#text-ex').css('opacity', 0);
 $('#text-ex').waypoint(function(){
-  $('#text-ex').addClass('bounceIn');
-}, { offset: '70%' });
+  $('#text-ex').addClass('fadeIn');
+}, { offset: '80%' });
 
 
 //equipo
 $('#item-1').css('opacity',0);
 $('#item-1').waypoint(function(){
-  $('#item-1').addClass('fadeInLeft');
-}, { offset: '70%'});
+  $('#item-1').addClass('fadeInUp');
+}, { offset: '80%'});
 
 $('#item-2').css('opacity',0);
 $('#item-2').waypoint(function(){
-  $('#item-2').addClass('fadeInLeft');
+  $('#item-2').addClass('fadeInUp');
 }, { offset: '70%'});
 
 $('#item-3').css('opacity',0);
 $('#item-3').waypoint(function(){
-  $('#item-3').addClass('fadeInRight');
+  $('#item-3').addClass('fadeInUp');
 }, { offset: '70%'});
 
 $('#item-4').css('opacity',0);
 $('#item-4').waypoint(function(){
-  $('#item-4').addClass('fadeInRight');
+  $('#item-4').addClass('fadeInUp');
 }, { offset: '70%'});
 
 //workshop
